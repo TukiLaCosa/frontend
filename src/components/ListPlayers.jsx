0b683bc4b9872a73879b1d120842711166b7f7f8
nehu <nehuen.aramburu@mi.unc.edu.ar>
2023-10-01 16:19:59 -0300

    Dibujitos generados aleatoriamente para la lista de nombres

--- src/components/ListPlayers.jsx
+++ src/components/ListPlayers.jsx
@@ -1,15 +1,21 @@
 'use client';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import Image from "next/image";
 
 function ListPlayers({players}){
-    
-    
+    const monstersContext = require.context('!@svgr/webpack!../monsters', false, /\.svg$/);
+    const monsters = monstersContext.keys().map(monstersContext);
+    function RandomMonster() {
+        const randomIndex = Math.floor(Math.random() * monsters.length);
+        const SelectedMonster = monsters[randomIndex].default;
+        return <SelectedMonster  className="monster-image"/>;
+      }
     return <div className="block mt-2 listPlayers" style={{ overflowY: 'auto' }}>
         {players?.map((player, index) => (
             <div className="box media" key={index}>
             <div className="media-left">
-            <figure className="image is-32x32">+☈+</figure>
+            <figure className="image is-64x64">
+            <RandomMonster/>
+            </figure>
             </div>
             <div className="media-content">
             <p className="content">{player.name}</p>
