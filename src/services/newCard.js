ec79f394c1ab10269f21f5b9cf5c83ceb47a3b0c
gabiHak <gabriel.hak@mi.unc.edu.ar>
2023-10-18 23:17:11 -0300

    Patch hecho

--- src/services/newCard.js
+++ src/services/newCard.js
@@ -1,8 +1,14 @@
+import axios from 'axios'
 import { addToHand } from './addToHand'
 
-export const newCard = (setCardsPlayer, setTurnState, turnState, turnStates) => {
-  if (turnState !== turnStates.DRAW) return
-  const random = Math.floor(Math.random() * (108 - 2 + 1) + 2)
-  addToHand(setCardsPlayer, random)
+export const newCard = (setCardsPlayer, setTurnState, turnState, turnStates, user, game) => {
+  const userId = user?.id
+  const gameName = game?.id
+  const body = { player_id: userId }
+  const url = `http://127.0.0.1/games/${gameName}/draw-card`
+  const response = axios.patch(url, body)
+  if (response?.ok && turnState === turnStates.DRAW) {
+    addToHand(setCardsPlayer, response?.card.id)
+  }
   setTurnState(turnStates.PLAY)
 }
