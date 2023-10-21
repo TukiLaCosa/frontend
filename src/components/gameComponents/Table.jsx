0da93ce9464f826efd61cc5a7454d0c018718ed2
Bitlogic <amparo.carnero@mi.unc.edu.ar>
2023-10-21 17:51:41 -0300

    Finalización de partida (falta un detalle)

--- src/components/gameComponents/Table.jsx
+++ src/components/gameComponents/Table.jsx
@@ -13,12 +13,15 @@ import {
   horizontalListSortingStrategy,
 } from "@dnd-kit/sortable";
 import { sortCards } from "@/services/sortCards";
-import { playCard,playFlamethrower,getAdjacents} from "@/services/playCard";
+import { useRouter } from 'next/navigation';
+import { playCard, playFlamethrower, getAdjacents } from "@/services/playCard";
 import { discardCard } from "@/services/discardCard";
 import { newCard } from "@/services/newCard";
 import { useUserGame } from "@/services/UserGameContext";
 import { useWebSocket } from "@/services/WebSocketContext";
 import axios from "axios";
+import { closeModal } from "../ExitAndStart";
+import '@/styles/game_ended.scss'
 
 export const handleDragEnd = (
   event,
@@ -63,6 +66,65 @@ export const fetchCards = async (user, setCardsPlayer) => {
   }
 };
 
+
+// FETCH PARA OBTENER LOS DATOS DE LA FINALIZACIÓN DE LA PARTIDA
+export const fetchResultsGame = async (gameName, setWinners, setLosers, setWasTheThing, setShowModal) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:8000/games/${gameName}/result`
+    );
+    console.log(response)
+    console.log(response.data)
+    const data = response.data; // obtiene el objeto
+    setWinners(data.winners.map(winner => winner.name));
+    setLosers(data.losers.map(loser => loser.name));
+
+    // Busca al jugador que era "La Cosa" y establece el estado en consecuencia
+    const theThingPlayer = data.winners.find(winner => winner.was_the_thing) ||
+      data.losers.find(loser => loser.was_the_thing);
+
+    setWasTheThing(theThingPlayer ? theThingPlayer.name : '');
+    openModal(setShowModal); // Abre el modal dsp de obtener los resultados
+  } catch (error) {
+    console.error("Error getting results:", error);
+  }
+};
+
+
+// BORRAR LA PARTIDA LUEGO DEL MODAL
+export const deleteGame = async (gameName, setShowModal) => {
+  closeModal(setShowModal) // cierra el modal
+  try {
+    const url = `http://localhost:8000/games/${gameName}`
+    const response = await axios.delete(url)
+    if (!response?.ok) {
+      console.log(response)
+    }
+    localStorage.removeItem('game')
+  } catch (error) {
+    console.error('Error deleting game:', error)
+  }
+}
+
+// para abrir un modal
+export const openModal = (setShowModal) => {
+  setShowModal(true)
+}
+
+// PEGARLE A UN ENDPOINT HARDCODEADO PARA FINALIZAR PARTIDA
+export const finalizarPartida = async (gameName) => {
+  console.log(gameName)
+  try {
+    const response = await axios.get( // hacer get para finalizar la partida. Luego de pegarle a este endpoint manda el evento de game_ended
+      `http://localhost:8000/ws/${gameName}/send-self-event?event=game_ended` // le indico qué evento quiero q dispare por query parameters
+    );
+    console.log(response);
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error WS:", error);
+  }
+};
+
 function Table() {
   const [cardsPlayer, setCardsPlayer] = useState([]);
   const [playBG, setPlayBG] = useState("/cards/rev/109Rev.png");
@@ -73,10 +135,15 @@ function Table() {
   const { user, game } = useUserGame();
   const { event } = useWebSocket();
   const [showFlamethrowerConfirmation, setShowFlamethrowerConfirmation] = useState(false);
-  const userId = user?.id
-  const [playingCardId,setPlayingCardId] = useState(0)
+  const userId = user?.id;
+  const [playingCardId, setPlayingCardId] = useState(0);
   const gameName = game?.name;
-  
+  const [showModal, setShowModal] = useState(false) // para mostrar el modal de los ganadores, perdedores, etc
+  const [winners, setWinners] = useState([]);
+  const [losers, setLosers] = useState([]);
+  const [wasTheThing, setWasTheThing] = useState(false);
+  const router = useRouter();
+
 
   useEffect(() => {
     const gameName = game?.name;
@@ -97,12 +164,16 @@ function Table() {
     const eventJSON = JSON.parse(event);
     const eventType = eventJSON?.event;
     if (eventType == "played_card") {
-      handlePlayedCardEvent(); // falta definir
+      handlePlayedCardEvent();
+    } else if (eventType === "game_ended") { // evento de finalizar partida
+      console.log("HOALAAAAa")
+      fetchResultsGame(gameName, setWinners, setLosers, setWasTheThing) // le pega al endpoint para obtener los resultados
+    } else if (eventType === "game_deleted") { // una vez q se hace el delete de la partida en la q estan todos, se manda este evento y es para que todos los usuarios salgas de ahi
+      console.log("HOLAAAA")
+      router.push('/search-game')
     }
   }, [event]);
 
-  //let id_victim1, id_victim2 = getAdjacents(players,userId)
-  
 
   return (
     <div className="table is-flex is-flex-direction-row">
@@ -113,12 +184,46 @@ function Table() {
             handleDragEnd(event, setCardsPlayer, setPlayBG, setDiscardBG, setShowFlamethrowerConfirmation, setPlayingCardId);
           }} // as onChange
         >
-          {showFlamethrowerConfirmation && (
-            <div className="confirmation-dialog">
-              <p>Pregunta: ¿A quien quieres quemar?</p>
-              <button onClick={() => setShowFlamethrowerConfirmation(false)}>Cancelar</button>
-              <button onClick={() => playFlamethrower(playingCardId, userId, 1, gameName)}>Jugador : </button> 
-              <button onClick={() => playFlamethrower(playingCardId, userId, 2, gameName)}>Jugador :</button>
+          <button
+            className='button is-success is-danger is-large'
+            onClick={() => {
+              finalizarPartida(gameName);
+              openModal(setShowModal); // abre el modal dsp de obtener los resultados
+            }}
+          >
+            Finzalizar Partidaaa
+          </button>
+          {showModal && (
+            <div className='modal is-active'>
+              <div className='modal-background' onClick={closeModal} />
+              <div className='modal-card'>
+                <header className='modal-card-head'>
+                  <p className="modal-card-title">La partida ha finalizado y estos son los resultados</p>
+                </header>
+                <section className="modal-card-body">
+                  <div>
+                    <h3 className="title">Ganadores:</h3>
+                    <ul className="ul">
+                      {winners.map((winner, index) => (
+                        <li key={index}>{winner}</li>
+                      ))}
+                    </ul>
+                  </div>
+                  <div>
+                    <h3 className="title">Perdedores:</h3>
+                    <ul className="ul">
+                      {losers.map((loser, index) => (
+                        <li key={index}>{loser}</li>
+                      ))}
+                    </ul>
+                  </div>
+                  <div>
+                    <h3 className="title">La Cosa:</h3>
+                    <p>{wasTheThing}</p>
+                  </div>
+                </section>
+                <button className='button is-success is-tuki' onClick={() => { deleteGame(gameName, setShowModal) }}>AFUERA</button>
+              </div>
             </div>
           )}
 
@@ -286,7 +391,7 @@ function Table() {
 
 
       </div>
-      
+
 
       <div
         className="chat has-text-centered column is-flex is-flex-direction-column is-justify-content-space-evenly"
