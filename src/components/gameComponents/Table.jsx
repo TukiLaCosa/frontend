245bede9d04c28ec0472ed45c02df99d00559e48
nehu <nehuen.aramburu@mi.unc.edu.ar>
2023-11-14 00:29:05 -0300

    no se puede descartar si se esta infectado y se tiene una unica carta infectado

--- src/components/gameComponents/Table.jsx
+++ src/components/gameComponents/Table.jsx
@@ -31,7 +31,9 @@ export const handleDragEnd = (event, turnState, user, game, setCardsPlayer, play
 
   if (over.id === 'discard-deck' && turnState === turnStates.PLAY) {
     // Discarding
-    if (active.id === 1) {
+    const cardsInfected = cardsPlayer.filter(objeto => objeto.id >= 2 && objeto.id <= 21)
+    if (active.id === 1 || (active.id >= 2 && active.id <= 21 &&
+      user?.status === 'INFECTED')) {
       setContentModal('No puedes descartar esta carta')
     } else {
       const handleFunction = (value) => {
